26a02abd555926c350510ce8049a0fa5914c5a3b
akmal <isakulovdev@gmail.com>
2026-01-22 15:22:06 +0500

    fix: app notifications page

--- src/ui/notifications.rs
+++ src/ui/notifications.rs
@@ -49,12 +49,11 @@ impl SimpleComponent for NotificationsModel {
                     set_title: "App Notifications",
 
                     adw::ActionRow {
-                        set_title: "HuaweiNetwork",
-                        set_subtitle: "Connected",
+                        set_title: "App Name",
                         set_activatable: true,
 
                         add_prefix = &gtk::Image {
-                            set_icon_name: Some("network-wireless-symbolic"),
+                            set_icon_name: Some("cluster"),
                             set_pixel_size: 16,
                         },
 
@@ -62,33 +61,15 @@ impl SimpleComponent for NotificationsModel {
                             set_orientation: gtk::Orientation::Horizontal,
                             set_spacing: 6,
 
-                            gtk::Button {
-                                set_icon_name: "qr-code-symbolic",
-                                add_css_class: "flat",
-                                set_valign: gtk::Align::Center,
+                            gtk::Label {
+                                set_label: "On"
                             },
 
                             gtk::Button {
-                                set_icon_name: "emblem-system-symbolic",
+                                set_icon_name: "go-next",
                                 add_css_class: "flat",
                                 set_valign: gtk::Align::Center,
-                            }
-                        }
-                    },
-
-                    adw::ActionRow {
-                        set_title: "Jamshidbek",
-                        set_activatable: true,
-
-                        add_prefix = &gtk::Image {
-                            set_icon_name: Some("network-wireless-symbolic"),
-                            set_pixel_size: 16,
-                        },
-
-                        add_suffix = &gtk::Button {
-                            set_icon_name: "emblem-system-symbolic",
-                            add_css_class: "flat",
-                            set_valign: gtk::Align::Center,
+                            },
                         }
                     },
                 }
