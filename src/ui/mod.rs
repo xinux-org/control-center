bc6c00568d84086abd67c9c9884c106acddf1f71
akmal <isakulovdev@gmail.com>
2026-01-22 15:24:59 +0500

    pull

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,10 +1,12 @@
 pub mod about;
+pub mod accounts;
 pub mod bluetooth;
 pub mod display;
 pub mod multitasking;
 pub mod network;
 pub mod notifications;
 pub mod power;
+pub mod sharing;
 pub mod sound;
 pub mod wifi;
 
