026240db65c21f5bbe8913b57722d8aec435eb27
akmal <isakulovdev@gmail.com>
2026-02-03 11:51:26 +0500

    minor changes

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -3,15 +3,16 @@ pub mod bluetooth;
 pub mod display;
 pub mod network;
 pub mod notifications;
-pub mod power;
 pub mod sound;
 pub mod wifi;
 
+// Pages in separate folders
 pub mod apps;
+pub mod power;
 pub mod search;
 
-pub mod multitasking;
 pub mod accounts;
+pub mod multitasking;
 pub mod sharing;
 pub mod wellbeing;
 
